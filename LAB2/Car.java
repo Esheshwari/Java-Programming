05812a96c12460f645c7b21661177366e880d931
Esheshwari Kumari <esheshwaritiwari@gmail.com>
2025-08-19 10:38:12 +0530

    Create Car.java

---
+++ LAB2/Car.java
@@ -0,0 +1,21 @@
+public class Car {
+    String brand;
+    int year;
+
+    // Constructor
+    Car(String b, int y) {
+        brand = b;
+        year = y;
+    }
+
+    void display() {
+        System.out.println("Car: " + brand + ", Year: " + year);
+    }
+
+    public static void main(String[] args) {
+        Car c1 = new Car("Toyota", 2020);
+        Car c2 = new Car("Honda", 2019);
+        c1.display();
+        c2.display();
+    }
+}
